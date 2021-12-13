a68f89638cd4deba7c7bae0d301222ccf4ee1b16
Victor Ferrando <victor.ferrando@hotmail.com>
2021-12-13 19:11:46 -0300

    update exercises

---
+++ src/exercicios/exercicio05/Pessoa.java
@@ -0,0 +1,35 @@
+package exercicios.exercicio05;
+
+public class Pessoa {
+
+	private String nome;
+	private int idade;
+	private float altura;
+
+	void Pessoa(String nome, int idade, float altura) {
+		this.nome = nome;
+		this.idade = idade;
+		this.altura = altura;
+	}
+
+	void armazenaPessoa(String nome, int idade, float altura) {
+
+	}
+
+	void removePessoa(String nome) {
+
+	}
+
+	//public int buscaPessoa(String nome) {
+		
+	//}
+	
+	void imprimeAgenda() {
+		
+	}
+	
+	void imprimePessoa(int index) {
+		
+	}
+
+}
