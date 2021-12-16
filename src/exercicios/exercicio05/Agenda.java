21792f76d75e8883d36a43910795dc95d0808735
Victor Ferrando <victor.ferrando@hotmail.com>
2021-12-16 16:30:55 -0300

    Adicionando exercicio 5

---
+++ src/exercicios/exercicio05/Agenda.java
@@ -0,0 +1,64 @@
+package exercicios.exercicio05;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Agenda {
+	Scanner entrada = new Scanner(System.in);
+	ArrayList<Pessoa> listaPessoa = new ArrayList<>();
+	int contador = 0;
+
+	public void armazenaPessoa(String nome, int idade, float altura) {
+		if (contador > 10) {
+			System.out.println("Você atingiu o máximo de 10 pessoas!");
+		} else {
+			listaPessoa.add(new Pessoa(nome, idade, altura));
+			contador++;
+		}
+
+	}
+
+	public int buscaPessoa(String nome) {
+		System.out.println("Digite o nome da pessoa que deseja procurar: ");
+		nome = entrada.nextLine();
+		int posicao = 0;
+
+		for (int i = 0; i <= listaPessoa.size(); i++) {
+			if (listaPessoa.contains(nome)) {
+				posicao = i;
+				break;
+			}
+		}
+		return posicao;
+	}
+
+	void imprimePessoa(int index) {
+		System.out.println("Digite a posição da agenda: ");
+		index = entrada.nextInt();
+
+		for (int i = 0; i <= listaPessoa.size(); i++) {
+			if (index == i) {
+				System.out.println(listaPessoa.get(i));
+			}
+
+		}
+
+	}
+
+	void imprimeAgenda() {
+		System.out.println(listaPessoa);
+	}
+
+	void removePessoa(String nome) {
+		System.out.println("Digite o nome da pessoa que deseja remover: ");
+		nome = entrada.nextLine();
+		
+		for (int i = 0; i <= listaPessoa.size(); i++) {
+			if (listaPessoa.contains(nome)) {
+				listaPessoa.remove(i);
+				break;
+			}
+	}
+
+}
+}
--- src/exercicios/exercicio05/Pessoa.java
+++ src/exercicios/exercicio05/Pessoa.java
@@ -5,31 +5,43 @@ public class Pessoa {
 	private String nome;
 	private int idade;
 	private float altura;
+	
 
-	void Pessoa(String nome, int idade, float altura) {
+	public Pessoa(String nome, int idade, float altura) {
 		this.nome = nome;
 		this.idade = idade;
 		this.altura = altura;
 	}
 
-	void armazenaPessoa(String nome, int idade, float altura) {
 
+	public String getNome() {
+		return nome;
 	}
 
-	void removePessoa(String nome) {
 
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
 
-	//public int buscaPessoa(String nome) {
-		
-	//}
+
+	public int getIdade() {
+		return idade;
+	}
+
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+
+	public float getAltura() {
+		return altura;
+	}
+
+
+	public void setAltura(float altura) {
+		this.altura = altura;
+	}
 	
-	void imprimeAgenda() {
-		
-	}
-	
-	void imprimePessoa(int index) {
-		
-	}
 
 }
